649a0851907a55eb551c9129cbc69343f684a3a4
godugubalesabu <godugubalesabu@gmail.com>
2023-11-01 19:32:36 +0530

    initial commit

---
+++ 001/app.js
@@ -0,0 +1,27 @@
+
+
+const demoElement = React.createElement(
+    "h1",
+    {id:"title",className:"title"},
+    "Indepth React| advanced");
+
+const parentDiv = React.createElement(
+    "div",
+    {id:"parent",className:"parent"},
+   [demoElement,demoElement]);
+
+const parentParentElement =React.createElement("div",{
+    id:"demoroot"
+   },parentDiv)
+
+
+
+
+
+
+
+
+console.log(parentParentElement);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parentParentElement);
